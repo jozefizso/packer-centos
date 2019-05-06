dd4202f6e6875885ebdd7984a0124fbff3db9435
Jozef Izso <jozef.izso@gmail.com>
2019-05-06 23:30:31 +0200

    Publish script for 1810.03 release

--- upload.cmd
+++ upload.cmd
@@ -4,7 +4,7 @@ vagrant cloud auth whoami
 
 vagrant cloud box create jozefizso/centos7 --short-description "CentOS 7 images for Vagrant"
 
-vagrant cloud version create jozefizso/centos7 1810.02
+vagrant cloud version create jozefizso/centos7 1810.03
 
-vagrant cloud provider create jozefizso/centos7 vmware_desktop 1810.02
-vagrant cloud provider upload jozefizso/centos7 vmware_desktop 1810.02 "boxes/centos-7.6.1810.02-x86_64.box"
+vagrant cloud provider create jozefizso/centos7 vmware_desktop 1810.03
+vagrant cloud provider upload jozefizso/centos7 vmware_desktop 1810.03 "boxes/centos-7.6.1810.03-x86_64.box"
